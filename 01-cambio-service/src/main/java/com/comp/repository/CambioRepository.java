7635606c19cbf33c5865769d2f79f0998705ff89
aluno <aluno@INTRAUECA.CEFET.COM>
2023-05-16 15:14:56 -0300

    att 16/05/2023

---
+++ 01-cambio-service/src/main/java/com/comp/repository/CambioRepository.java
@@ -0,0 +1,11 @@
+package com.comp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.comp.model.Cambio;
+
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+	Cambio findByFromAndTo(String from, String to);
+
+}
