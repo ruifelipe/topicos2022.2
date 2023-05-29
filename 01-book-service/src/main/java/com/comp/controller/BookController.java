d751120a85470967a8644437b3aa0fa8c0793149
aluno <aluno@INTRAUECA.CEFET.COM>
2023-05-29 14:25:33 -0300

    att

--- 01-book-service/src/main/java/com/comp/controller/BookController.java
+++ 01-book-service/src/main/java/com/comp/controller/BookController.java
@@ -15,24 +15,26 @@ import com.comp.repository.BookRepository;
 @RestController
 @RequestMapping("book-service")
 public class BookController {
-
+	
 	@Autowired
 	private Environment environment;
-
+	
 	@Autowired
 	private BookRepository repository;
-
-	// http://localhost:8100/book-service/1/BRL
-	@GetMapping(value = "/{id}/{currency}")
-	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
-
-		var book = repository.getById(id);
-
-		if(book == null) throw new RuntimeException("Book Not Found");
-				
+	
+	//http://localhost:8100/book-service/1/BRL
+	@GetMapping(value="/{id}/{currency}")
+	public Book findBook(
+			@PathVariable("id") Long id,
+            @PathVariable("currency") String currency
+			) {
+		
+		var book = repository.findById(id).get();
+		if (book==null) throw new RuntimeException("Book Not Found");
+		
 		String port = environment.getProperty("local.server.port");
 		book.setEvironment(port);
-
+		
 		return book;
 	}
 
--- 01-book-service/src/main/java/com/comp/model/Book.java
+++ 01-book-service/src/main/java/com/comp/model/Book.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,6 +16,7 @@ import jakarta.persistence.TemporalType;
 import jakarta.persistence.Transient;
 
 @Entity(name="book")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book implements Serializable{
 
 	private static final long serialVersionUID = 1L;
